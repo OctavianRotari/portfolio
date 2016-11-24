115c04370aa337b82ac2574b28561341f4257a57
Octavian Rotari <octavianrotari@gmail.com>
2016-11-24 16:33:51 +0100

    header (wip)

---
+++ resources/js/typed.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+  $(".js--text-box").typed({
+    strings: ["Persist. Practice. Experiment. Imagine.", "Drink losts of coffee.", "Do your best work and all rest will follow!"],
+    typeSpeed: 50,
+    startDelay: 500,
+  });
+});
