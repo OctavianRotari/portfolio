69c9e04780a6e3641541b45ad6834d405510264a
Octavian Rotari <octavianrotari@gmail.com>
2016-11-28 18:15:50 +0100

    changed the menu

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -1,25 +1,12 @@
 $(document).ready(function() {
   'use strict';
 
-  // On click, remove class on active element, add it to the new one
   $('.js--main-nav li').click(function(e) {
-    var id = parseInt(this.id);
-    $('.js--main-nav li.active').removeClass('active');
-    $(this).addClass('active');
-
     // Scroll to index
+    var id = parseInt(this.id);
     $(".js--main").moveTo(id);
     e.preventDefault();
     return false;
-  });
-
-  // On scroll, remove class on active element and add it to the new one
-  $(".js--main").onepage_scroll({
-    beforeMove: function(index) {
-      $('.js--main-nav li.active').removeClass('active');
-      var element = document.getElementById(index);
-      $(element).addClass('active');
-    }
   });
 });
 
