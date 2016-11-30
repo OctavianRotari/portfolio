16311de0ea152f05c0318d2de7cb50163560356a
Octavian Rotari <octavianrotari@gmail.com>
2016-11-30 19:07:53 +0100

    traveler design(wip)

---
+++ resources/js/carousel.js
@@ -0,0 +1,4 @@
+$(document).ready(function(){
+  $('#ca-container-languages').contentcarousel({direction: 1});
+  $('#ca-container-frameworks').contentcarousel({direction: -1});
+});
--- vendors/js/jquery.contentcarousel.js
+++ vendors/js/jquery.contentcarousel.js
@@ -1,11 +1,11 @@
 (function($) {
-  var	aux		= {
+  var aux = {
     // navigates left / right
-    navigate	: function( dir, $el, $wrapper, opts, cache ) {
+    navigate: function( dir, $el, $wrapper, opts, cache ) {
 
-      var scroll		= opts.scroll,
-        factor		= 1,
-        idxClicked	= 0;
+      var scroll = opts.scroll,
+        factor = 1,
+        idxClicked = 0;
 
       if( cache.expanded ) {
         scroll		= 1; // scroll is always 1 in full mode
@@ -18,10 +18,8 @@
         $wrapper.find('div.ca-item:lt(' + scroll + ')').each(function(i) {
           $(this).clone(true).css( 'left', ( cache.totalItems - idxClicked + i ) * cache.itemW * factor + 'px' ).appendTo( $wrapper );
         });
-      }
-      else {
+      } else {
         var $first	= $wrapper.children().eq(0);
-
         $wrapper.find('div.ca-item:gt(' + ( cache.totalItems  - 1 - scroll ) + ')').each(function(i) {
           // insert before $first so they stay in the right order
           $(this).clone(true).css( 'left', - ( scroll - i + idxClicked ) * cache.itemW * factor + 'px' ).insertBefore( $first );
@@ -31,9 +29,9 @@
       // animate the left of each item
       // the calculations are dependent on dir and on the cache.expanded value
       $wrapper.find('div.ca-item').each(function(i) {
-        var $item	= $(this);
+        var $item = $(this);
         $item.stop().animate({
-          left	:  ( dir === 1 ) ? '-=' + ( cache.itemW * factor * scroll ) + 'px' : '+=' + ( cache.itemW * factor * scroll ) + 'px'
+          left:  ( dir === 1 ) ? '-=' + ( cache.itemW * factor * scroll ) + 'px' : '+=' + ( cache.itemW * factor * scroll ) + 'px'
         }, opts.sliderSpeed, opts.sliderEasing, function() {
           if( ( dir === 1 && $item.position().left < - idxClicked * cache.itemW * factor ) || ( dir === -1 && $item.position().left > ( ( cache.totalItems - 1 - idxClicked ) * cache.itemW * factor ) ) ) {
             // remove the item that was cloned
@@ -45,27 +43,18 @@
 
     },
 
-    // gets the item's position (1, 2, or 3) on the viewport (the visible items)
-    // val is the left of the item
-    getWinPos	: function( val, cache ) {
-      switch( val ) {
-        case 0 					: return 1; break;
-        case cache.itemW 		: return 2; break;
-        case cache.itemW * 2 	: return 3; break;
-      }
-    }
   },
     methods = {
-      init 		: function( options ) {
+      init: function( options ) {
 
         if( this.length ) {
 
           var settings = {
-            sliderSpeed		: 500,			// speed for the sliding animation
-            sliderEasing	: 'easeOutExpo',// easing for the sliding animation
-            itemSpeed		: 500,			// speed for the item animation (open / close)
-            itemEasing		: 'easeOutExpo',// easing for the item animation (open / close)
-            scroll			: 1				// number of items to scroll at a time
+            sliderSpeed: 500,// speed for the sliding animation
+            sliderEasing: 'easeOutExpo',// easing for the sliding animation
+            itemSpeed	: 500,// speed for the item animation (open / close)
+            itemEasing: 'easeOutExpo',// easing for the item animation (open / close)
+            scroll: 1, // number of items to scroll at a time
           };
 
           return this.each(function() {
@@ -79,24 +68,19 @@
               $wrapper = $el.find('div.ca-wrapper'),
               $items = $wrapper.children('div.ca-item'),
               cache = {};
+              left = 1;
+              right = -1;
 
             // save the with of one item
-            cache.itemW			= $items.width();
+            cache.itemW = $items.width();
             // save the number of total items
-            cache.totalItems	= $items.length;
-
-            // add navigation buttons
-            if( cache.totalItems > 3 )
-              $el.prepend('<div class="ca-nav"><span class="ca-nav-prev">Previous</span><span class="ca-nav-next">Next</span></div>')
+            cache.totalItems = $items.length;
 
             // control the scroll value
             if( settings.scroll < 1 )
               settings.scroll = 1;
             else if( settings.scroll > 3 )
               settings.scroll = 3;
-
-            var $navPrev		= $el.find('span.ca-nav-prev'),
-              $navNext		= $el.find('span.ca-nav-next');
 
             // hide the items except the first 3
             $wrapper.css( 'overflow', 'hidden' );
@@ -110,25 +94,11 @@
               });
             });
 
-            // navigate left
-            $navPrev.bind('click.contentcarousel', function( event ) {
-              if( cache.isAnimating ) return false;
-              cache.isAnimating	= true;
-              aux.navigate( -1, $el, $wrapper, settings, cache );
-            });
-
-            // navigate right
-            $navNext.bind('click.contentcarousel', function( event ) {
-              if( cache.isAnimating ) return false;
-              cache.isAnimating	= true;
-              aux.navigate( 1, $el, $wrapper, settings, cache );
-            });
-
             window.setInterval(function(){
               if( cache.isAnimating ) return false;
               cache.isAnimating = true;
-              aux.navigate( 1, $el, $wrapper, settings, cache );
-            }, 5000);
+              aux.navigate(settings.direction, $el, $wrapper, settings, cache );
+            }, 3000);
 
           });
         }
@@ -137,7 +107,7 @@
 
   $.fn.contentcarousel = function(method) {
     if ( methods[method] ) {
-      return methods[method].apply( this, Array.prototype.slice.call( arguments, 1 ));
+      return methods[method].apply( this, Array.prototype.slice.call( arguments, -1 ));
     } else if ( typeof method === 'object' || ! method ) {
       return methods.init.apply( this, arguments );
     } else {
