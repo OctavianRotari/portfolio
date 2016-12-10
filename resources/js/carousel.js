aa810033b34a5574adae86dc8f666f54c90f8ca0
Octavian Rotari <octavianrotari@gmail.com>
2016-12-10 17:54:57 +0100

    styling developer page

--- resources/js/carousel.js
+++
@@ -1,4 +0,0 @@
-$(document).ready(function(){
-  $('#ca-container-languages').contentcarousel({direction: 1});
-  $('#ca-container-frameworks').contentcarousel({direction: -1});
-});
---
+++ resources/js/popup.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+  $('.test-popup-link').magnificPopup({
+    type: 'image'
+  });
+})
