b6a68cb81eae6d7a89013806dd62a95a152b3334
Octavian Rotari <octavianrotari@gmail.com>
2016-12-20 18:39:12 +0100

    styling the whole

--- resources/js/typed.js
+++ resources/js/typed.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   $(".js--text-box").typed({
-    strings: ["Persist. Practice. Experiment. Imagine.", "Drink losts of coffee.", "Do your best work and all rest will follow!"],
+    strings: ["Persist. Practice. Experiment. Imagine.<br> Do your best work and all rest will follow!"],
     typeSpeed: 50,
     startDelay: 500,
   });
