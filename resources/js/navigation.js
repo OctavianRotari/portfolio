1bd73bebc4ab32a24a8066ba59c83e40c762457e
Octavian Rotari <octavianrotari@gmail.com>
2017-01-16 22:19:37 +0100

    fullpage js (WIP)

--- resources/js/navigation.js
+++ resources/js/navigation.js
@@ -1,11 +1,11 @@
-$(document).ready(function() {
-  'use strict';
-  $('.js--main-nav li').click(function(e) {
-    // Scroll to index
-    var id = parseInt(this.id);
-    $(".js--main").moveTo(id);
-    e.preventDefault();
-    return false;
-  });
-});
+//$(document).ready(function() {
+  //'use strict';
+  //$('.js--main-nav li').click(function(e) {
+    //// Scroll to index
+    //var id = parseInt(this.id);
+    //$.fn.fullpage.moveTo(id);
+    //e.preventDefault();
+    //return false;
+  //});
+//});
 
--- resources/js/particleground.js
+++ resources/js/particleground.js
@@ -1,13 +1,5 @@
 $(document).ready(function(){
-  $('#js--summary-who-am-i').particleground({
-    dotColor: '#005882',
-    lineColor: '#005882'
-  });
-  $('section').particleground({
-    dotColor: '#005882',
-    lineColor: '#005882'
-  });
-  $('.horizontal').particleground({
+  $('.section').particleground({
     dotColor: '#005882',
     lineColor: '#005882'
   });
