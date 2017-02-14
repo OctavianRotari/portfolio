27140ff59147ab3b2dd5b1365da14bb7b5d3d3eb
Octavian Rotari <octavianrotari@gmail.com>
2017-02-14 13:26:38 +0100

    delay type

--- resources/js/typed.js
+++ resources/js/typed.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
   $(".js--text-box").typed({
     strings: ["Persist. Practice. Experiment. Imagine.", "Do your best work and all rest will follow!"],
     typeSpeed: 50,
-    startDelay: 500,
+    startDelay: 1000,
   });
 });
