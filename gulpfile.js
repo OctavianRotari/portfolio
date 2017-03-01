41628ceb0703f23d44683666a3a38f523e2793de
Octavian Rotari <octavianrotari@gmail.com>
2017-03-01 20:53:18 +0000

    fix bug google fonts

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,61 @@
 var gulp = require('gulp');
-var server = require('gulp-server-livereload');
+var serverLiveReload = require('gulp-server-livereload');
 var cleanCss = require('gulp-clean-css');
 var sass = require('gulp-sass');
 var googleWebFonts = require('gulp-google-webfonts');
 var concat = require('gulp-concat');
+var autoprefixer = require('gulp-autoprefixer');
+var clean = require('gulp-clean');
 //var debug = require('gulp-debug');
 
 var options = {
-  fontsDir: 'fonts',
+  fontsDir: 'dist/css/fonts',
+  fontsPath: 'fonts/',
+  cssDir: 'src/vendors/css',
   cssFilename: 'googleWebFonts.css'
 };
 
-gulp.task('style', function () {
-  gulp.src([ 'src/vendors/css/*.css', 'src/resources/scss/*.{scss,css}' ])
+var cssFiles = [
+  'src/vendors/css/googleWebFonts.css',
+  'src/vendors/css/*.css',
+  'src/resources/scss/*.{scss,css}'
+];
+
+function cleanDist() {
+  return gulp.src([ 'dist/css/fonts/', 'dist/css/fonts/*.css' ])
+  .pipe(clean());
+}
+
+function fonts() {
+  return gulp.src('src/resources/fonts/fonts.list')
+    .pipe(googleWebFonts(options))
+    .pipe(gulp.dest('./'));
+}
+
+function style() {
+  return gulp.src(cssFiles)
     .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
     .pipe(cleanCss({ compatibility: 'ie8' }))
     .pipe(concat('style.css'))
     .pipe(gulp.dest('dist/css'));
-});
+}
 
-gulp.task('fonts', function () {
-  gulp.src('src/resources/fonts/fonts.list')
-    .pipe(googleWebFonts(options))
-  //.pipe(debug({ title: 'fonts' }))
-    .pipe(cleanCss({ compatibility: 'ie8' }))
-    .pipe(gulp.dest('dist/css/'));
-});
+function watch() {
+  gulp.watch(cssFiles, style);
+  gulp.watch('src/resources/fonts/fonts.list', fonts);
+}
 
-gulp.task('watch', function () {
-  gulp.watch('src/resources/css/style.scss', [ 'style' ]);
-  gulp.watch('src/resources/fonts/fonts.list', [ 'fonts' ]);
-});
-
-gulp.task('server', function () {
-  gulp.src('./')
-    .pipe(server({
-      livereload: true,
+function server() {
+  return gulp.src('./')
+    .pipe(serverLiveReload({
       directoryListing: true,
       open: true
     }));
-});
+}
 
-gulp.task('default',[ 'style', 'fonts', 'watch', 'server' ]);
+var build = gulp.series(cleanDist, fonts, style, gulp.parallel(watch, server));
+
+gulp.task('default', build);
+
+gulp.task('fonts', fonts);
